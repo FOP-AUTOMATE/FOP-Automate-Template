69ff97fa8fc0d3759758095332494d46f1a47686
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-23 20:07:39 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,78 @@
 #!/bin/bash
 
+echo ""
+echo ""
+
+echo "###############################################"
+echo "#                                             #"
+echo "#  Welcome to the FOP Workspace Setup Script  #"
+echo "#                                             #"
+echo "###############################################"
+
 while true; do
+    echo "How should the repository be named (remotely, on github)?"
+    read REPO_NAME
+
+    echo "Is this correct? (y/n)"
+    echo "Repository name: $REPO_NAME"
+    read yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) ;;
+        * ) echo "Please answer y/n";;
+    esac
+done
+
+while true; do
+    echo "How should the repository be named (locally, on your computer)?"
+
+    read REPO_DIR
+
+    echo "Is this correct? (y/n)"
+    echo "Repository name: $REPO_DIR"
+    read yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) ;;
+        * ) echo "Please answer y/n";;
+    esac
+done
+
+# check if repo exists on github
+if gh repo view $REPO_NAME > /dev/null 2>&1; then
+    echo "Repository $REPO_NAME already exists on github. Exiting."
+    exit 1
+fi
+
+# check if repo exists locally
+if [ -d $REPO_DIR ]; then
+    echo "Repository $REPO_DIR already exists locally. Exiting."
+    exit 1
+fi
+
+# Clone from github
+git clone https://github.com/nsc-de/FOP-Automate-Template.git $REPO_DIR
+cd TU-FOP-Workspace
+git remote rename origin from
+
+while true; do
+
+    echo ""
+    echo ""
         
+    echo ""
     echo "Please enter your student id:"
     read STUDENT_ID
 
+    echo ""
     echo "Please enter your first name:"
     read FIRST_NAME
 
+    echo ""
     echo "Please enter your last name:"
     read LAST_NAME
 
+    echo ""
     echo "Please enter your github username:"
     read GITHUB_USERNAME
 
@@ -50,4 +112,15 @@ echo "REPO_DIR_PREFIX=TU-" >> settings.properties
 
 echo "Saved settings to settings.properties"
 
+git add settings.properties
+git commit -m "Added settings.properties"
 
+gh repo create $REPO_NAME --private
+git remote add origin https://github.com/$GITHUB_USERNAME/$REPO_NAME.git
+git push --set-upstream origin main --force
+
+echo ""
+echo ""
+
+echo "Created repository $REPO_NAME on your github account"
+echo "See https://github.com/$GITHUB_USERNAME/$REPO_NAME"
