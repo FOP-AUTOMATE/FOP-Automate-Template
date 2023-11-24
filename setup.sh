299222c224ad2abe9cdbb79d1bb0d348ab8e43b2
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-24 18:27:41 +0100

    Create test workflow

--- setup.sh
+++ setup.sh
@@ -11,7 +11,13 @@ echo "###############################################"
 echo ""
 echo ""
 
-while true; do
+# Check if REPO_NAME is given as environment variable
+
+if [[ -n "$REPO_NAME" ]]; then
+  REPO_NAME=$REPO_NAME
+else
+
+  while true; do
     echo ""
     echo ""
     echo "How should the repository be named (remotely, on github)?"
@@ -24,13 +30,19 @@ while true; do
     echo "Is this correct? (y/n)"
     read yn
     case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) ;;
-        * ) echo "Please answer y/n";;
+    [Yy]*) break ;;
+    [Nn]*) ;;
+    *) echo "Please answer y/n" ;;
     esac
-done
+  done
+fi
 
-while true; do
+# Check if REPO_DIR is given as environment variable
+
+if [[ -n "$REPO_DIR" ]]; then
+  REPO_DIR=$REPO_DIR
+else
+  while true; do
     echo ""
     echo ""
     echo "How should the repository be named (locally, on your computer)?"
@@ -44,22 +56,23 @@ while true; do
     echo "Is this correct? (y/n)"
     read yn
     case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) ;;
-        * ) echo "Please answer y/n";;
+    [Yy]*) break ;;
+    [Nn]*) ;;
+    *) echo "Please answer y/n" ;;
     esac
-done
+  done
+fi
 
 # check if repo exists on github
-if gh repo view $REPO_NAME > /dev/null 2>&1; then
-    echo "Repository $REPO_NAME already exists on github. Exiting."
-    exit 1
+if gh repo view $REPO_NAME >/dev/null 2>&1; then
+  echo "Repository $REPO_NAME already exists on github. Exiting."
+  exit 1
 fi
 
 # check if repo exists locally
 if [ -d $REPO_DIR ]; then
-    echo "Repository $REPO_DIR already exists locally. Exiting."
-    exit 1
+  echo "Repository $REPO_DIR already exists locally. Exiting."
+  exit 1
 fi
 
 # Clone from github
@@ -67,60 +80,117 @@ git clone https://github.com/nsc-de/FOP-Automate-Template.git $REPO_DIR
 cd $REPO_DIR
 git remote rename origin from
 
-while true; do
+# Get STUDENT_ID
 
+if [[ -n "$STUDENT_ID" ]]; then
+  STUDENT_ID=$STUDENT_ID
+else
+  while true; do
     echo ""
-    echo ""
-        
     echo ""
     echo "Please enter your student id:"
     read STUDENT_ID
 
     echo ""
+    echo ""
+
+    echo "Student id: $STUDENT_ID"
+    echo "Is this correct? (y/n)"
+    read yn
+    case $yn in
+    [Yy]*) break ;;
+    [Nn]*) ;;
+    *) echo "Please answer y/n" ;;
+    esac
+  done
+fi
+
+# Get FIRST_NAME
+
+if [[ -n "$FIRST_NAME" ]]; then
+  FIRST_NAME=$FIRST_NAME
+else
+  while true; do
+    echo ""
+    echo ""
     echo "Please enter your first name:"
     read FIRST_NAME
 
+    echo ""
+    echo ""
+
+    echo "First name: $FIRST_NAME"
+    echo "Is this correct? (y/n)"
+    read yn
+    case $yn in
+    [Yy]*) break ;;
+    [Nn]*) ;;
+    *) echo "Please answer y/n" ;;
+    esac
+  done
+fi
+
+# Get LAST_NAME
+
+if [[ -n "$LAST_NAME" ]]; then
+  LAST_NAME=$LAST_NAME
+else
+  while true; do
+    echo ""
     echo ""
     echo "Please enter your last name:"
     read LAST_NAME
 
     echo ""
+    echo ""
+
+    echo "Last name: $LAST_NAME"
+    echo "Is this correct? (y/n)"
+    read yn
+    case $yn in
+    [Yy]*) break ;;
+    [Nn]*) ;;
+    *) echo "Please answer y/n" ;;
+    esac
+  done
+fi
+
+# Get GITHUB_USERNAME
+
+if [[ -n "$GITHUB_USERNAME" ]]; then
+  GITHUB_USERNAME=$GITHUB_USERNAME
+else
+  while true; do
+    echo ""
+    echo ""
     echo "Please enter your github username:"
     read GITHUB_USERNAME
 
-    # Save the settings to a file
-    echo "STUDENT_ID=$STUDENT_ID"
-    echo "FIRST_NAME=$FIRST_NAME"
-    echo "LAST_NAME=$LAST_NAME"
-    echo "GITHUB_USERNAME=$GITHUB_USERNAME"
+    echo ""
+    echo ""
 
-    while true; do
-        echo "Is this correct? (y/n)"
-        read yn
-        case $yn in
-            [Yy]* ) break;;
-            [Nn]* ) break;;
-            * ) echo "Please answer y/n";;
-        esac
-    done
-
+    echo "Github username: $GITHUB_USERNAME"
+    echo "Is this correct? (y/n)"
+    read yn
     case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) ;;
+    [Yy]*) break ;;
+    [Nn]*) ;;
+    *) echo "Please answer y/n" ;;
     esac
-done
+  done
+fi
 
 # Save to settings.properties
-echo "# Settings" > settings.properties
-echo "" >> settings.properties
-echo "STUDENT_ID=$STUDENT_ID" > settings.properties
-echo "FIRST_NAME=$FIRST_NAME" >> settings.properties
-echo "LAST_NAME=$LAST_NAME" >> settings.properties
-echo "GITHUB_USERNAME=$GITHUB_USERNAME" >> settings.properties
-echo "" >> settings.properties
-echo "PROVIDER_GIT=https://github.com/FOP-2324" >> settings.properties
-echo "REPO_PREFIX=TU-" >> settings.properties
-echo "REPO_DIR_PREFIX=TU-" >> settings.properties
+echo "# Settings" >settings.properties
+echo "" >>settings.properties
+echo "STUDENT_ID=$STUDENT_ID" >settings.properties
+echo "FIRST_NAME=$FIRST_NAME" >>settings.properties
+echo "LAST_NAME=$LAST_NAME" >>settings.properties
+echo "GITHUB_USERNAME=$GITHUB_USERNAME" >>settings.properties
+echo "" >>settings.properties
+echo "PROVIDER_GIT=https://github.com/FOP-2324" >>settings.properties
+echo "REPO_PREFIX=TU-" >>settings.properties
+echo "REPO_DIR_PREFIX=TU-" >>settings.properties
 
 echo "Saved settings to settings.properties"
 
